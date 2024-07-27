e88cd9dbcc28699e371167288429f02a2403b101
Durgesh Singh <57593854+DurgeshSingh1@users.noreply.github.com>
2024-07-27 18:16:49 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -65,7 +65,7 @@ router.get("/logout",async (req,res)=>{
 //REFETCH USER
 router.get("/refetch", (req,res)=>{
     const token=req.cookies.token
-    jwt.verify(token,process.env.SECRET,{},async (err,data)=>{
+    jwt.verify(token,process.env.SECRET,async (err,data)=>{
         if(err){
             return res.status(404).json(err)
         }
