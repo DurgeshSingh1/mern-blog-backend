48a5611c8d3145c8729e3fe011597dde3cbcabf6
Durgesh Singh <57593854+DurgeshSingh1@users.noreply.github.com>
2024-07-27 18:15:13 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -27,17 +27,20 @@ router.post("/register",async(req,res)=>{
 router.post("/login",async (req,res)=>{
     try{
         const user=await User.findOne({email:req.body.email})
-       
+        console.log("user - " + user);
         if(!user){
             return res.status(404).json("User not found!")
         }
         const match=await bcrypt.compare(req.body.password,user.password)
-        
+        console.log("match - " + match);
         if(!match){
             return res.status(401).json("Wrong credentials!")
         }
         const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
+        console.log("token - " + token);
         const {password,...info}=user._doc
+        console.log("password - " + password);
+        console.log("info - " + info);
         res.cookie("token",token).status(200).json(info)
 
     }
